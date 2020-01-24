e17686000216d557590efacac6da6018819f91ed
PhilomenaGMIT <G00387837@gmit.ie>
2020-01-24 16:52:00 +0000

    update to bmi calculation program just to add comments

--- bmicalc.py
+++ bmicalc.py
@@ -1,8 +1,12 @@
 # This program calculates a person's body mass index from their height and weight.
-#The preson must enter their height in cms and weight in kg and the porgram outputs the bmi in metres squared
+#The preson must enter their height in cms and weight in kg and the porgram calculates and outputs their BMI 
 
 height = float(input("Please enter your height in cms: "))
 weight = float(input("Please enter your weight in kgs: "))
+
+# convert height to metres 
 heightM = height/100
+
 bmi = float (weight / (heightM ** 2))
+
 print("Thank You. Your BMI has been calculated as ",bmi)
